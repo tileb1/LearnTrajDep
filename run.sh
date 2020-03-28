a63f4d3b6009968c43b752640c02e83b466c03d1
Tim <tim.lebailly@gmail.com>
2020-03-28 23:46:24 +0100

    added bash file

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-python main.py --input_n 10 --output_n 25 --dct_n 35 --data_dir [PATH TO YOUR H36M DATASET]
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+    eval `ssh-agent`
+    ssh-add
+fi
+
+git pull
+source ../anaconda3/bin/activate
+conda activate maowei
+
+CUDA_VISIBLE_DEVICES=0 python main.py --epoch 5 --input_n 10 --output 10 --dct_n 20 --data_dir h3.6m/dataset/
+ 
+
+git add .
+git commit -m "auto commit from server after last run"
+git push
